765ad6cc47117bb9c04a057e5b278b69f1e3d438
jcezar-js <julio.filho1@hotmail.com>
2021-08-20 22:10:55 -0300

    Aula3 - intent e comunicação entre páginas

--- Android/AppUrlsFavoritas/app/src/main/java/br/com/up/appurlsfavoritas/MainActivity.java
+++ Android/AppUrlsFavoritas/app/src/main/java/br/com/up/appurlsfavoritas/MainActivity.java
@@ -1,7 +1,10 @@
 package br.com.up.appurlsfavoritas;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -18,16 +21,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         fabAddLink = findViewById(R.id.fab_main_add_link);
-        fabAddLink.setImageResource(R.drawable.ic_launcher_foreground);
+//       fabAddLink.setImageResource(R.drawable.ic_launcher_foreground);
 
-        View.OnClickListener listener = new View.OnClickListener() {
+//        View.OnClickListener listener = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getBaseContext(), "Clicou", Toast.LENGTH_LONG).show();
+//            }
+//        };
+
+
+        fabAddLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getBaseContext(), "Clicou", Toast.LENGTH_LONG).show();
+                Toast.makeText(getBaseContext(), "Abrindo Register", Toast.LENGTH_LONG).show();
+//                callBrowserWithUrl("https://www.netflix.com");
+                callRegisterActivity();
             }
-        };
-
-
-        fabAddLink.setOnClickListener(listener);
+        });
     }
+
+    private void callRegisterActivity() {
+
+        Intent intent = new Intent(this, RegisterActivity.class);
+
+        startActivityForResult(intent, 1234);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1234 && resultCode == RESULT_OK){
+            String url = data.getStringExtra("link");
+            Toast.makeText(getBaseContext(), "URL " + url, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Função para chamar o navegador e entrar na netflix
+    private void callBrowserWithUrl(String url){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
 }
--- Android/AppUrlsFavoritas/app/src/main/java/br/com/up/appurlsfavoritas/RegisterActivity.java
+++ Android/AppUrlsFavoritas/app/src/main/java/br/com/up/appurlsfavoritas/RegisterActivity.java
@@ -2,13 +2,49 @@ package br.com.up.appurlsfavoritas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+
+    private Button buttonSave;
+
+    private TextInputEditText inputEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        buttonSave = findViewById(R.id.button_register_save);
+        inputEditText = findViewById(R.id.input_register_link);
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                saveLink();
+            }
+        });
+    }
+    private void saveLink() {
+        String link = inputEditText.getEditableText().toString();
+        if (link.isEmpty()) {
+            inputEditText.setError("Favor verificar o link digitado.");
+        }else{
+
+            Intent intent  = new Intent();
+            intent.putExtra("Link", link);
+            setResult(RESULT_OK,intent);
+            finish();
+        }
     }
 }
