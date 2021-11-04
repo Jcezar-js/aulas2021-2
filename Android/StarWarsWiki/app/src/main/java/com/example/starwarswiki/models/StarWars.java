3cb94b9db0e5524b848ecad8479d82247f9db7d8
Jcezar-js <julio.filho1@hotmail.com>
2021-11-03 21:43:45 -0300

    StarWarsWiki

---
+++ Android/StarWarsWiki/app/src/main/java/com/example/starwarswiki/models/StarWars.java
@@ -0,0 +1,14 @@
+package com.example.starwarswiki.models;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class StarWars implements Serializable
+{
+    private int id;
+    private String name;
+    private String image;
+    private ArrayList<>
+
+
+}
